bdea19b80990ccf3a7789ded1ccac2752f27baf0
Leonardo Segala <lallo.segala@gmail.com>
2024-11-09 04:52:39 +0100

    CVE requests are cached

--- scanner/cve/fetch_cves.py
+++ scanner/cve/fetch_cves.py
@@ -1,6 +1,8 @@
 
 import aiohttp
 import asyncio
+import os
+import json
 
 URL = "https://services.nvd.nist.gov/rest/json/cves/2.0?cpeName={cpe}&isVulnerable"
 
@@ -9,6 +11,25 @@ async def download_cve(cpe: str) -> dict:
         async with session.get(URL.format(cpe=cpe)) as response:
             response.raise_for_status()
             return await response.json()
+
+async def get_cve(cpe: str, use_cache: bool = True) -> dict:
+
+    if use_cache:
+        filename = os.path.join(".cache", "cached_" + cpe.replace("/", "_"))
+
+        if os.path.exists(filename):
+            with open(filename) as fp:
+                return json.load(fp)
+
+    cve = await download_cve(cpe)
+
+    if use_cache:
+        os.makedirs(".cache", exist_ok=True)
+
+        with open(filename, "w") as fp:
+            json.dump(cve, fp)
+
+    return cve
 
 async def interactive_cve_fetch(cpe_list: list[str], queue: asyncio.Queue, max_tasks: int) -> dict:
 
@@ -21,7 +42,7 @@ async def interactive_cve_fetch(cpe_list: list[str], queue: asyncio.Queue, max_t
 
             await queue.put({"agent": "cve", "type": "cve download start", "cpe": cpe})
 
-            cve[cpe] = await download_cve(cpe)
+            cve[cpe] = await get_cve(cpe)
 
             await queue.put({"agent": "cve", "type": "cve download complete", "cpe": cpe})
 
